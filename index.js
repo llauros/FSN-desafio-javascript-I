238973a809b33a9ac37c79d70eefdc7a64ad534a
Lucas Lauro <62807645+llauros@users.noreply.github.com>
2020-08-04 13:15:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 // Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
-
-
-// implementação
-const nomeEscola = "DH"
-
-//var alunosDaEscola = [];
 const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},
 {nome:"Edson",notas:[],cursos:[],faltas:2},
 {nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},
 {nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},
 {nome:"Carlos",notas:[],cursos:[],faltas:0},
 {nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
+
+// implementação
+const nomeEscola = "DH"
 
 // Registro de Alunos
 function adicionarAluno(nomeAluno){
